3dbdd56d75ec16b96bd8f92d4bf81a43928f0e6b
박진억 <cramis@gmail.com>
2018-12-20 11:51:28 +0900

    엑셀 Export  적용

---
+++ Ipsi/Controllers/ExcelController.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace Ipsi.Controllers
+{
+    public class ExcelController : Controller
+    {
+        private IHostingEnvironment _hostingEnvironment;
+        public ExcelController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+
+        public async Task<IActionResult> Export()
+        {
+            string sWebRootFolder = _hostingEnvironment.WebRootPath;
+            string sFileName = @"demo.xlsx";
+            string URL = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, sFileName);
+            FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+            var memory = new MemoryStream();
+            using (var fs = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook workbook;
+                workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet("Demo");
+                IRow row = excelSheet.CreateRow(0);
+
+                row.CreateCell(0).SetCellValue("ID");
+                row.CreateCell(1).SetCellValue("Name");
+                row.CreateCell(2).SetCellValue("Age");
+
+                row = excelSheet.CreateRow(1);
+                row.CreateCell(0).SetCellValue(1);
+                row.CreateCell(1).SetCellValue("Kane Williamson");
+                row.CreateCell(2).SetCellValue(29);
+
+                row = excelSheet.CreateRow(2);
+                row.CreateCell(0).SetCellValue(2);
+                row.CreateCell(1).SetCellValue("Martin Guptil");
+                row.CreateCell(2).SetCellValue(33);
+
+                row = excelSheet.CreateRow(3);
+                row.CreateCell(0).SetCellValue(3);
+                row.CreateCell(1).SetCellValue("Colin Munro");
+                row.CreateCell(2).SetCellValue(23);
+
+                workbook.Write(fs);
+            }
+            using (var stream = new FileStream(Path.Combine(sWebRootFolder, sFileName), FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", sFileName);
+        }
+    }
+}
